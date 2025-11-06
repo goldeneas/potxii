927a49492e2b907f2574a24a810d5dffdbfd371e
Nicola <nicola.picarella6@gmail.com>
2025-11-06 13:48:35 +0100

    fix ledpwm float to int

--- led_pwm.py
+++ led_pwm.py
@@ -6,6 +6,12 @@ class LedPWM:
         self._max_pwm_value = max_pwm_value
         self._pwm = PWM(self._pin)
 
+    def off(self):
+        self.set_brightness(0)
+        
+    def on(self):
+        self.set_brightness(100)
+
     # value between 0 and 100
     def set_brightness(self, value=50):
-        self._pwm.duty(value / 100 * self._max_pwm_value)
+        self._pwm.duty(int(value / 100 * self._max_pwm_value))
