7e3c17e7acd81ef7a3f12004a6fd637dff18e1e2
Nick0471 <nicolomassimolisena@gmail.com>
2025-11-27 13:27:27 +0100

    Modifica mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -1,40 +1,55 @@
 import paho.mqtt.client as mqtt
 import time
 
-# The callback for when the client receives a
-# CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    global loop_flag
-    print("Connected with result code "+str(rc))
-    print("\n connected with client "+ str(client))
-    print("\n connected with userdata "+str(userdata))
-    print("\n connected with flags "+str(flags))
-    loop_flag=0
-    
-def on_disconnect():
-    print("\nDisconnected") #da modificare
-    
-def on_subscribe(client, userdata, msg, qos_l):
-    print("\non_sub: client ="+str(client))
-    print("\non_sub: msg ="+str(msg))
-    print("\non_sub: qos level ="+str(qos_l))
-    
-def on_unsubscribe():
-    print("\nUnsubscribed") #da modificare
-    
-def on_publish(client,userdata,result): #create function for callback
-    print("data published \n")
-    print("client = "+ str(client))
-    print("\nresult in on_publish= ", result)
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("\n on message: "+msg.topic+" "+str(msg.payload))
-    
-def on_log(client, userdata, level, buf):
-    print("\n log:client = "+ str(client))
-    print("\n log:level ="+str(level))
-    print("\n log:buffer "+str(buf))
-    
+class MQTT:
+    def __init__(self, nome_client):
+        self.client = mqtt.Client(nome_client)
+        
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_subscibe = self.on_subscribe
+        self.client.on_publish = self.on_publish
+        self.client.on_message = self.on_message
+        self.client.on_log = self.on_log
+               
+        self.client.connect("test.mosquitto.org", 1883, 60)
+        
+    # The callback for when the client receives a
+    # CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc):
+        global loop_flag
+        print("Connected with result code "+str(rc))
+        print("\n connected with client "+ str(client))
+        print("\n connected with userdata "+str(userdata))
+        print("\n connected with flags "+str(flags))
+        loop_flag=0
+        
+    def on_disconnect():
+        print("\nDisconnected") #da modificare
+        
+    def on_subscribe(client, userdata, msg, qos_l):
+        print("\non_sub: client ="+str(client))
+        print("\non_sub: msg ="+str(msg))
+        print("\non_sub: qos level ="+str(qos_l))
+        
+    def on_unsubscribe():
+        print("\nUnsubscribed") #da modificare
+        
+    def on_publish(client,userdata,result): #create function for callback
+        print("data published \n")
+        print("client = "+ str(client))
+        print("\nresult in on_publish= ", result)
 
-    
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(client, userdata, msg):
+        print("\n on message: "+msg.topic+" "+str(msg.payload))
+        
+    def on_log(client, userdata, level, buf):
+        print("\n log:client = "+ str(client))
+        print("\n log:level ="+str(level))
+        print("\n log:buffer "+str(buf))
+        
+
+        
+
