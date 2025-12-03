035e95df5a1478d0fa60724eef056724384eb9dc
Nicola <nicola.picarella6@gmail.com>
2025-12-03 14:57:41 +0100

    removed warning screen

--- src/warning_screen.py
+++
@@ -1,9 +0,0 @@
-from ssd1306 import SSD1306_I2C
-
-class WarningScreen:
-    def __init__(self, display: SSD1306_I2C):
-        self.display = display
-
-
-    def show(self):
-        self.text_all(self.warning_messages)
