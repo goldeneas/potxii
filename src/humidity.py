a1f4559ab064e59210ab041a4cb64f0e83517c37
Nicola <nicola.picarella6@gmail.com>
2025-12-19 12:05:54 +0100

    CHANGED

--- src/humidity.py
+++ src/humidity.py
@@ -1,36 +1,16 @@
-from machine import Pin, ADC
+from machine import Pin
+from machine import ADC
 
-class Humidity:
-    def __init__(self, pin, max=4095, min=2500):
-        #max=4095 il sensore legge umidità 0% (non è inserito nel terreno)
-        #min=2500 il sensore legge umidità 100% (è inserito completamente in acqua)
-     
-        #A0
-        self.adc = ADC(Pin(pin))
-        self.adc.atten(ADC.ATTN_11DB) # Range 0-3.3V
-        
-      
-        self.val_max = max
-        self.val_min = min
-        
+class Humidity():
+    def __init__(self, pin_number):
+        pin = Pin(pin_number)
+        self.__adc = ADC(pin)
+        self.__adc.atten(ADC.ATTN_11DB)
 
     def read(self):
-        #Restituisce solo il numero puro (0-4095)
-        return self.adc.read()
+        adc_reading = self.__adc.read()
+        return adc_reading
 
-
-    def value(self):
-        #Restituisce la percentuale calcolata (0-100%)
-        reading = self.read()
-                
-       
-        # Serve per evitare di avere -5% o 105% se la calibrazione non è perfetta
-        calculated = reading
-        if calculated > self.val_max: calculated = self.val_max
-        if calculated < self.val_min: calculated = self.val_min
-        
-        
-        # 2. Matematica (Inversione e conversione in %)
-        perc = (self.val_max - calculated) / (self.val_max - self.val_min) * 100
-        
-        return int(perc)
+    def value(self, adc_max = 4095):
+        adc_reading = self.read()
+        return (100 * adc_reading) / adc_max
