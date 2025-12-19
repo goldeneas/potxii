4dc09611205dbd2d6fc969c180e5e495cc98b767
Nicola <nicola.picarella6@gmail.com>
2025-12-19 11:56:13 +0100

    changed humidity

--- src/humidity.py
+++ src/humidity.py
@@ -1,27 +1,36 @@
 from machine import Pin, ADC
 
-class Humidity():
-    def __init__(self, pin_number, min_val=1500, max_val=4095):
-        # min_val: Valore letto in acqua (massima umidità)
-        # max_val: Valore letto in aria (minima umidità)
-        pin = Pin(pin_number)
-        self.__adc = ADC(pin)
-        self.__adc.atten(ADC.ATTN_11DB) # Range 0-3.3V (circa)
-        self.min_val = min_val
-        self.max_val = max_val
+class Humidity:
+    def __init__(self, pin, max=4095, min=2500):
+        #max=4095 il sensore legge umidità 0% (non è inserito nel terreno)
+        #min=2500 il sensore legge umidità 100% (è inserito completamente in acqua)
+     
+        #A0
+        self.adc = ADC(Pin(pin))
+        self.adc.atten(ADC.ATTN_11DB) # Range 0-3.3V
+        
+      
+        self.val_max = max
+        self.val_min = min
+        
 
     def read(self):
-        return self.__adc.read()
+        #Restituisce solo il numero puro (0-4095)
+        return self.adc.read()
+
 
     def value(self):
-        adc_reading = self.read()
+        #Restituisce la percentuale calcolata (0-100%)
+        reading = self.read()
+                
+       
+        # Serve per evitare di avere -5% o 105% se la calibrazione non è perfetta
+        calculated = reading
+        if calculated > self.val_max: calculated = self.val_max
+        if calculated < self.val_min: calculated = self.val_min
         
-        # Mappa il valore letto tra 0% e 100% usando i limiti reali
-        # La logica è inversa: lettura alta = secco, lettura bassa = bagnato
-        if adc_reading >= self.max_val:
-            return 0
-        elif adc_reading <= self.min_val:
-            return 100
-        else:
-            # Formula di mappatura lineare inversa
-            return (self.max_val - adc_reading) / (self.max_val - self.min_val) * 100
+        
+        # 2. Matematica (Inversione e conversione in %)
+        perc = (self.val_max - calculated) / (self.val_max - self.val_min) * 100
+        
+        return int(perc)
