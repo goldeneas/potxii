4c10e477bd7bca040acb23a78a705263b324acd0
Nicola <nicola.picarella6@gmail.com>
2025-12-21 11:04:36 +0100

    changed humidity

--- src/humidity.py
+++ src/humidity.py
@@ -1,27 +1,25 @@
 from machine import Pin, ADC
 
 class Humidity():
-    def __init__(self, pin_number, min_val=1500, max_val=4095):
+    def __init__(self, pin_number, min_val=0, max_val=100):
         # min_val: Valore letto in acqua (massima umidità)
         # max_val: Valore letto in aria (minima umidità)
         pin = Pin(pin_number)
         self.__adc = ADC(pin)
-        self.__adc.atten(ADC.ATTN_11DB) # Range 0-3.3V (circa)
         self.min_val = min_val
         self.max_val = max_val
 
     def read(self):
+        """
+        Read a raw value from the LDR.
+        :return a value from 0 to 4095.
+        """
         return self.__adc.read()
 
     def value(self):
-        adc_reading = self.read()
-        
-        # Mappa il valore letto tra 0% e 100% usando i limiti reali
-        # La logica è inversa: lettura alta = secco, lettura bassa = bagnato
-        if adc_reading >= self.max_val:
-            return 0
-        elif adc_reading <= self.min_val:
-            return 100
-        else:
-            # Formula di mappatura lineare inversa
-            return (self.max_val - adc_reading) / (self.max_val - self.min_val) * 100
+        """
+        Read a value from the LDR in the specified range.
+        :return a value from the specified [min, max] range.
+        """
+        return (self.max_val - self.min_val) * self.read() / 4095
+
